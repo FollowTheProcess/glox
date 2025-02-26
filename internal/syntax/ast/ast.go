1f5cb3ae7b250f74444f2ee41b9fc57d8acb348e
Tom Fleet <tomfleet2018@gmail.com>
2025-02-26 21:12:13 +0000

    Some operator precedence tests

--- internal/syntax/ast/ast.go
+++ internal/syntax/ast/ast.go
@@ -1,7 +1,9 @@
 // Package ast implements the abstract syntax tree for Lox.
 package ast
 
-import "github.com/FollowTheProcess/glox/internal/syntax/token"
+import (
+	"github.com/FollowTheProcess/glox/internal/syntax/token"
+)
 
 // TODO(@FollowTheProcess): Make all AST nodes pretty print themselves and then tests can just be
 // comparing a diff of the produced strings, possibly even in .txtar files with automatic snapshot
