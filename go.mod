e0bd51efb93260b86f3d5c9174bad54476f25c50
Tom Fleet <tomfleet2018@gmail.com>
2025-03-02 14:03:51 +0000

    Add parse pipeline integration test framework

--- go.mod
+++ go.mod
@@ -7,10 +7,13 @@ require (
 	github.com/FollowTheProcess/msg v1.6.0
 	github.com/FollowTheProcess/test v0.20.3
 	github.com/FollowTheProcess/txtar v0.5.0
+	github.com/kr/pretty v0.3.1
 )
 
 require (
 	github.com/FollowTheProcess/hue v0.5.1 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/term v0.29.0 // indirect
 )
--- internal/syntax/syntax_test.go
+++ internal/syntax/syntax_test.go
@@ -1,6 +1,94 @@
 package syntax_test
 
-// TODO(@FollowTheProcess): Integration tests that test the parse pipeline in phases using txtar archives
-// lex: Take raw (valid) Lox source code as an entire program in src.lox, then tokenise and validate against tokens.txt
-// parse: Take the tokens from the lex phase, parse and produce an AST, serialise it and dump to parsed.txt
-// eval: Take the AST from parse and run it through the interpreter, dumping the raw string output to output.txt
+import (
+	"flag"
+	"io"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/FollowTheProcess/glox/internal/syntax/lexer"
+	"github.com/FollowTheProcess/glox/internal/syntax/parser"
+	"github.com/FollowTheProcess/glox/internal/syntax/token"
+	"github.com/FollowTheProcess/test"
+	"github.com/FollowTheProcess/txtar"
+	"github.com/kr/pretty"
+)
+
+var update = flag.Bool("update", false, "Update snapshots and testdata")
+
+// TODO(@FollowTheProcess): Many more test cases
+
+func TestParsePipeline(t *testing.T) {
+	validCaseGlob := filepath.Join("testdata", "valid", "*.txtar")
+
+	validCases, err := filepath.Glob(validCaseGlob)
+	test.Ok(t, err)
+
+	for _, file := range validCases {
+		name := filepath.Base(file)
+		t.Run(name, func(t *testing.T) {
+			archive, err := txtar.ParseFile(file)
+			test.Ok(t, err)
+
+			src, err := archive.Read("src.lox")
+			test.Ok(t, err)
+
+			expectedTokens, err := archive.Read("tokens.txt")
+			test.Ok(t, err)
+
+			expectedTree, err := archive.Read("parsed.txt")
+			test.Ok(t, err)
+
+			tokens := lex(src)
+			tree := parse(t, name, src)
+
+			if *update {
+				// Update the expected with what's actually been seen
+				err := archive.Write("tokens.txt", tokens)
+				test.Ok(t, err)
+
+				err = archive.Write("parsed.txt", tree)
+				test.Ok(t, err)
+
+				err = txtar.DumpFile(file, archive)
+				test.Ok(t, err)
+				return
+			}
+
+			// Test the tokens
+			test.Diff(t, tokens, expectedTokens)
+
+			// And the AST
+			test.Diff(t, tree, expectedTree)
+		})
+	}
+}
+
+// lex tokenises src, emitting a string of newline separated tokens.
+func lex(src string) string {
+	var tokens strings.Builder
+
+	lexer := lexer.New(src)
+	for {
+		tok := lexer.NextToken()
+		tokens.WriteString(tok.String() + "\n")
+		if tok.Is(token.EOF) {
+			break
+		}
+	}
+
+	return tokens.String()
+}
+
+// parse parses a stream of tokens into an AST, returning it's formatted
+// string representation.
+func parse(t *testing.T, name, src string) string {
+	t.Helper()
+
+	p := parser.New(name, src, false, io.Discard)
+	program, err := p.Parse()
+	test.Ok(t, err)
+
+	return pretty.Sprintf("%# v\n", program)
+}
